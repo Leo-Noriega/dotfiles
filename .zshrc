b2ab9843233e94f85f90911bacc57fad61ff7d98
Leo-Noriega <leonoriega100@gmail.com>
2025-01-21 18:21:34 -0600

    feat: Functions separated from my .zshrc

--- .zshrc
+++ .zshrc
@@ -2,39 +2,34 @@
 autoload -Uz vcs_info
 zstyle ':vcs_info:git:*' formats '(%b)'
 precmd() { vcs_info }
-
 setopt prompt_subst
 
 # ==== EXPORTS ====
+export LANG="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 export PATH="$HOME/.config/emacs/bin:$PATH"
 export PROMPT='%B%F{124}%t%f%b - %B%F{31}%n%f%b:%~ | %B%F{208}${vcs_info_msg_0_} %B%F{white}'
-export EDITOR="emacsclient -c"
+export EDITOR="nvim"
 export TERMINAL="alacritty"
+export PATH="/opt/homebrew/opt/unzip/bin:$PATH"
+export NVM_DIR="$HOME/.nvm"
+export MANPAGER="nvim +Man!"
 
 # ==== ALIASES ====
+alias l="ls"
+alias ll="ls -lah"
 alias et="emacsclient -t"
 alias pp="php bin/console"
+alias n=$EDITOR
+
 # ==== FUNCTIONS ====
+autoload -Uz compinit
+compinit
+for func_file in ~/.config/zshFunctions/*; do
+    source $func_file
+done
 
-# fzf into a directory
-cc() {
-    dir=$(fd -td -H . $HOME -E Library/ | fzf -i --border --padding=1 --height=30% --layout=reverse)
-    if [ -z "$dir" ]; then
-        gum style \
-            --foreground 31 --border rounded --align center --bold --padding "0.1 0.1" \
-            'No directory selected'
-    fi
-    cd "$dir"
-}
+# ==== LOADINGS ====
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# fzf into a file
-cf() {
-    file=$(fd -t f -H . $HOME -E Library/ | fzf)
-    if [ -z "$file" ]; then
-        gum style \
-            --foreground 31 --border rounded --align center --bold --padding "0.1 0.1" \
-            'No file selected'
-    fi
-    # gum choose --header="What are you gonna do with the file?" "emacs" "read" "finder"
-    $EDITOR "$file"
-}
---
+++ zshFunctions/cdFuncs.zsh
@@ -0,0 +1,100 @@
+# fzf into a directory
+cc() {
+    dir=$(fd -td -H . $HOME -E Library/ | fzf -i --border --padding=1 --height=30% --layout=reverse)
+    if [ -z "$dir" ]; then
+        gum style \
+            --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+            'No directory selected'
+        return 1
+    fi
+    cd "$dir" && gum style \
+        --foreground "#90A959" --border rounded --align center --bold --padding "0.1 0.1" \
+        "Now in: $(pwd)"
+}
+
+# fzf into a directory where you at
+c() {
+    dir=$(pwd | fd -td -d1 -H | fzf -i --border --padding=1 --height=30% --layout=reverse)
+    if [ -z "$dir" ]; then
+        gum style \
+            --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+            'No directory selected'
+        return 1
+    fi
+    cd "$dir" && gum style \
+        --foreground "#90A959" --border rounded --align center --bold --padding "0.1 0.1" \
+        "Now in: $(pwd)" }
+
+# fzf into a directory and open it (choose)
+opd() {
+    dir=$(fd -td -H . $HOME -E Library/ | fzf -i --border --padding=1 --height=30% --layout=reverse)
+    if [ -z "$dir" ]; then
+        gum style \
+            --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+            'No directory selected'
+        return 1
+    fi
+    opt=$(gum choose --header="What are you gonna do with the directory?" "nvim" "code" "finder" "zip")
+    case $opt in
+        nvim)
+            cd "$dir" && nvim "$dir"
+            ;;
+        code)
+            code "$dir"
+            ;;
+        finder)
+            open "$dir"
+            ;;
+        zip)
+            zip_name="$(basename "$dir").zip"
+            zip -r "$zip_name" "$dir" > /dev/null
+            if [ $? -eq 0 ]; then
+                gum style \
+                    --foreground "#90A959" --border rounded --align center --bold --padding "0.1 0.1" \
+                    "Zipped: $zip_name"
+            else
+                gum style \
+                    --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+                    "Failed to create zip file"
+            fi
+            ;;
+        *)
+            gum style \
+                --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+                'No option selected'
+            return 1
+            ;;
+    esac
+}
+
+# fzf into a file and open it (choose)
+opf() {
+    file=$(fd -t f -H . $HOME -E Library/ | fzf -i --border --padding=1 --height=30% --layout=reverse)
+    if [ -z "$file" ]; then
+        gum style \
+            --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+            'No file selected'
+        return 1
+    fi
+    opt=$(gum choose --header="What are you gonna do with the file?" "nvim" "cat" "copy")
+    case $opt in
+        nvim)
+            nvim "$file"
+            ;;
+        cat)
+            cat "$file"
+            ;;
+        copy)
+            cat "$file" | pbcopy
+            gum style \
+                --foreground "#90A959" --border rounded --align center --bold --padding "0.1 0.1" \
+                "Copied: $file"
+            ;;
+        *)
+            gum style \
+                --foreground "#AF1B01" --border rounded --align center --bold --padding "0.1 0.1" \
+                'No option selected'
+            return 1
+            ;;
+    esac
+}
